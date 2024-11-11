334d7a7ae71c718a941498f1c2100744d77ebc3b
Tblack6 <166353163+Tblack6@users.noreply.github.com>
2024-11-10 23:31:18 -0600

    FINAL new_user.php

--- new_user.php
+++ new_user.php
@@ -1,17 +1,62 @@
 <?php
-// session start here...
+// Start session
+session_start();
 
-// get all 3 strings from the form (and scrub w/ validation function)
+// Check if the form was submitted
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    
+    $input_username = $_POST['user'];
+    $input_password = $_POST['pwd'];
 
-// make sure that the two password values match!
+    // Login to the softball database
+    $servername = "localhost";
+    $db_username = "root";
+    $db_password = "";
+    $dbname = "softball";
 
+    // Connect to the database
+    $conn = new mysqli($servername, $db_username, $db_password, $dbname);
 
-// create the password_hash using the PASSWORD_DEFAULT argument
+    // Check connection
+    if ($conn->connect_error) {
+        die("Connection failed: " . $conn->connect_error);
+    }
 
-// login to the database
+    // Select password from users where username = <what the user typed in>
+    $sql = "SELECT password FROM users WHERE username = ?";
+    $stmt = $conn->prepare($sql);
+    $stmt->bind_param("s", $input_username);
+    $stmt->execute();
+    $stmt->store_result();
 
-// make sure that the new user is not already in the database
+    // If no rows, then username is not valid (but don't tell Mallory), just send her back to the login
+    if ($stmt->num_rows > 0) {
+        
+        $stmt->bind_result($db_password_hash);
+        $stmt->fetch();
 
-// insert username and password hash into db (put the username in the session
-// or make them login)
+        // password_verify(password from form, password from db)
+        if (password_verify($input_password, $db_password_hash)) {
+            // If good, put username in session
+            $_SESSION['username'] = $input_username;
+            
+            header("location: games.php");
+            exit;
+        } else {
+            // Otherwise send back to login
+            header("location: index.php");
+            exit;
+        }
+    } else {
+       
+        header("location: index.php");
+        exit;
+    }
 
+    // Close statement and connection
+    $stmt->close();
+    $conn->close();
+} else {
+    header("location: index.php");
+    exit;
+}
